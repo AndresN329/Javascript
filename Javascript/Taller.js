d590ede5847314d42eef25167ada7f1be863ca37
AndresN329 <andresnieblesmartinez@gmail.com>
2025-06-16 13:24:23 -0500

    Add files via upload

---
+++ Javascript/Taller.js
@@ -0,0 +1,41 @@
+// Exploring JavaScript
+
+//2.1
+let name = "Andres";
+console.log("Hi," + nombre);
+
+//2.2
+let whole = 7;
+let decimal = 2.2;
+let string = "Text string";
+console.log(whole);
+console.log(decimal);
+console.log(string);
+
+//2.3
+const PI = 3.1416;
+// const PI = 3.1415;
+
+
+//2.4
+//let worthless = ;
+
+//2.5
+let VarNull = null;
+let boolean = true;
+
+//Data Input and Output
+//3.1
+let age = prompt("Please enter your age:");
+console.log(`you are ${age} years old`);
+
+//3.2 
+alert("Welcome to this web page");
+
+//3.3
+let answer = confirm("Do you want to continue?");
+console.log("Answer:", answer);
+
+//Operators
+//4.1
+let 
