a142cfd417307643bc6f1bc0b1e022c05425e642
AndresN329 <andresnieblesmartinez@gmail.com>
2025-06-17 13:32:21 -0500

    Add files via upload

---
+++ Javascript/Exercise-easy.js
@@ -0,0 +1,23 @@
+let fruits = ["Apple", "Pear", "Pineapple"];
+fruits.shift(); //Delete last items or like that
+console.log(fruits);
+
+//Exercise #2
+
+let numbers = [2, 3, 5, 6, 10];
+let NewNumber = 1;
+
+numbers.push(NewNumber);
+console.log(numbers);
+
+//Exercise #3
+let Word = ["One"]
+let FirstWord = "No";
+
+Word.unshift(FirstWord);
+console.log(Word);
+
+//Exercise #4
+let colors = ["blue", "red", "yellow"];
+let LastColor = colors.pop();
+console.log(colors);
