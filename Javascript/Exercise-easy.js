b6ec4a5d3422bb35f8a43c27790d176afca3da01
AndresN329 <andresnieblesmartinez@gmail.com>
2025-06-18 13:30:08 -0500

    Update Exercise-easy.js

--- Javascript/Exercise-easy.js
+++ Javascript/Exercise-easy.js
@@ -1,9 +1,8 @@
 let fruits = ["Apple", "Pear", "Pineapple"];
-fruits.shift(); //Delete last items or like that
+fruits.shift(); //Delete first items or like that
 console.log(fruits);
 
 //Exercise #2
-
 let numbers = [2, 3, 5, 6, 10];
 let NewNumber = 1;
 
@@ -21,3 +20,20 @@ console.log(Word);
 let colors = ["blue", "red", "yellow"];
 let LastColor = colors.pop();
 console.log(colors);
+
+
+//For Loop
+let sentence = "This is a sentence"
+for(let word of sentence){console.log(word)}
+
+
+//exer 2
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+//Exer 3
+let palabra = "hola";
+for (let i in palabra) {
+  console.log(`Posición: ${i}, Carácter: ${palabra[i]}`);
+}
